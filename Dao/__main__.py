7e7816107816de309554e77c7d337be92c049464
Kent Clark <123401423+imnotyoursuperman@users.noreply.github.com>
2024-03-09 21:22:20 +0530

    Update __main__.py

--- Dao/__main__.py
+++ Dao/__main__.py
@@ -1,1 +1,46 @@
+import asyncio
+import contextlib
+import importlib
+import json
+import re
+import time
+import traceback
+from platform import python_version
+from random import choice
 
+import psutil
+import pyrogram
+import telegram
+import telethon
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.constants import ParseMode
+from telegram.error import (
+    BadRequest,
+    ChatMigrated,
+    Forbidden,
+    NetworkError,
+    TelegramError,
+    TimedOut,
+)
+from telegram.ext import (
+    ApplicationHandlerStop,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+from telegram.helpers import escape_markdown
+
+from Dao import (
+    BOT_NAME,
+    LOGGER,
+    OWNER_ID,
+    SUPPORT_CHAT,
+    TOKEN,
+    StartTime,
+    app,
+    dispatcher,
+    function,
+    loop,
+    tbot,
