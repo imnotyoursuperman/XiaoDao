80bb89e3d8bd6942d7c4711e96df41c9171edf08
Kent Clark <123401423+imnotyoursuperman@users.noreply.github.com>
2024-03-09 18:45:46 +0530

    Update config.py

--- Dao/config.py
+++ Dao/config.py
@@ -24,10 +24,13 @@ SQL_URL = ""
 #MongoDB database url
 MONGO_URL = ""
 
-#support chat link
+#Anything is fine but don't change it after 
+DB_NAME = ""
+
+#Support Chat link
 SUPPORT_CHAT = ""
 
-#log channel id, reply to a forwarded msg from the channel with /id in Rose Bot
+#Log Channel ID, reply to a forwarded msg from the channel with /id in Rose Bot
 LOG_CHANNEL = ""
 
 
